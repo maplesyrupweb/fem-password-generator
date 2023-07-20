4beaeb98ae8236ec5c4e1600a228e8f5eb8c23bd
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2023-07-19 23:42:00 -0700

    Add copy to clipboard function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
 console.log("hello world");
+
+
+function generatePassword() {
+
+ let length = document.getElementById("charLength").value;
+
+    console.log(length);
+
+    document.getElementById("theLength").innerHTML = length;
+
+    const characters = 'abcdefghijklmnopqrstuvwxyz';
+    let result = ' ';
+    const charactersLength = characters.length;
+    for(let i = 0; i < length; i++) {
+        result += 
+        characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    console.log("The answer is: " + result);
+    document.getElementById("target").innerHTML = result   
+}
+
+let text = document.getElementById('myText').innerHTML;
+  const copyContent = async () => {
+    try {
+      await navigator.clipboard.writeText(text);
+      console.log("The copied text is: " + text);
+    } catch (error) {
+      console.error('Issue with copy to clipboard: ', error);
+    }
+  }
